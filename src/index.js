07dd5330abf039bd4e68c2cf7cc3f26e14fa974f
Fernando Kendi <fedked@outlook.com>
2022-03-16 15:22:31 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const rootElement = document.getElementById('root');
+const App = () => {
+    return <h1>Olá Mundo dev</h1>
+}
+
+ReactDOM.render(<App />, rootElement)
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "/public/index.html"),
+    }),
+  ],
+};
